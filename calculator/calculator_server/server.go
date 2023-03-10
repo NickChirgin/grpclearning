e7e579db3812f197fc7e69ff33ea6f6128499a73
Nikolay <nickchirgin@gmail.com>
2023-03-10 13:33:22 +0300

    added another bidi proto and server grpc

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -30,6 +30,28 @@ func (s *server) Prime(req *calculatorpb.PrimeRequest, stream calculatorpb.Prime
 	}
 	return nil
 }
+func (s *server) MaxInteger(stream calculatorpb.PrimeService_MaxIntegerServer) error {
+	fmt.Println("Max integer server on the line")
+	m := int64(0)
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while recieving stream %v", err)
+			return err
+		}
+		m = max(m, req.GetNumber())
+		err = stream.Send(&calculatorpb.MaxIntegerResponse{
+			Max: m,
+		})
+		if err != nil {
+			log.Fatalf("Error while sending max %v", err)
+			return err
+		}
+	}
+}
 func (*server) ComputeAverage(stream calculatorpb.PrimeService_ComputeAverageServer) error {
 	fmt.Println("Compute average server")
 	result, count := 0, 0
@@ -60,4 +82,10 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
 }
